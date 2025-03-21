06dbbfa22aeef0ff35d6053dfc0c0f607d265b71
Antonio <antoniomontanaro@Host-007.homenet.telecomitalia.it>
2025-03-21 14:10:55 +0100

    Ignore ESLint errors during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  // disabilita controllo errori lint in fase di build
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  // ...altre config
 };
 
 export default nextConfig;
